d2a5755db0eb9174543dbb705202d6aac35d9f41
Vicios579 <Vinicius.guedes@sptech.school>
2025-10-22 12:10:23 -0300

    teste

---
+++ Site Aggran/main.js
@@ -0,0 +1,29 @@
+// var senhas = [];
+// var usuarios = [];
+
+
+
+// function cadastrar() {
+//   var usuario = document.getElementById(input_user);
+//   var senha = document.getElementById(input_senha);
+//   usuario = input_user.value
+//   senha = input_senha.value
+
+//   usuarios.push(usuario);
+//   senhas.push(senha);
+//   localStorage.setItem(usuarios);
+// }
+
+// function login() {
+//   var usuario = document.getElementById(input_user_login);
+//   var senha = document.getElementById(input_senha_login);
+//   usuario = input_user_login.value
+//   senha = input_senha_login.value
+
+//   const valor = localStorage.getItem(usuarios);
+  
+//   if (usuarios.includes(usuario)) {
+//     alert ('oi')
+//   }
+    
+// }
