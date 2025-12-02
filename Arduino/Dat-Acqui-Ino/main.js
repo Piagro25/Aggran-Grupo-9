9d12c5d4d44cb6cc99874310c00e3cf97437851d
gustavoVbdr <gustavo.rodrigues@sptech.school>
2025-12-02 13:47:39 -0300

    dat-acqui confing p/ red'

--- Arduino/Dat-Acqui-Ino/main.js
+++ Arduino/Dat-Acqui-Ino/main.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 
 // constantes para configurações
 const SERIAL_BAUD_RATE = 9600;
-const SERVIDOR_PORTA = 3300;
+const SERVIDOR_PORTA = 3309;
 
 // habilita ou desabilita a inserção de dados no banco de dados
 const HABILITAR_OPERACAO_INSERIR = true;
@@ -19,10 +19,10 @@ const serial = async (
     // conexão com o banco de dados MySQL
     let poolBancoDados = mysql.createPool(
         {
-            host: 'localhost',
+            host: '10.18.32.170',
             user: 'aluno',
             password: 'Sptech#2024',
-            database: 'Aggran',
+            database: 'aggran',
             port: 3307
         }
     ).promise();
